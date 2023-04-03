d3fc121210d88123741c9b80e07d3e3a5d3254be
Denis Leonov <d3liaz@gmail.com>
2023-04-03 12:51:45 +0200

    Fix plausible config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ export default function App({Component, pageProps}: FullAppProps) {
 
   return (
     <SessionProvider session={pageProps.session}>
-      <PlausibleProvider domain="trackerjam.com" customDomain="p.hostedapp.one">
+      <PlausibleProvider domain="trackerjam.com" customDomain="https://p.hostedapp.one">
         <StyletronProvider value={styletron}>
           <BaseProvider theme={Theme}>
             <Layout>
