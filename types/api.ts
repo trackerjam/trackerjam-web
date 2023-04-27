2445929ba5d2202c2f3c0add5260922b301c925f
Denis Leonov <d3liaz@gmail.com>
2023-04-27 23:38:29 +0200

    Implement public API for Member

--- types/api.ts
+++ types/api.ts
@@ -1,4 +1,5 @@
 import {Session} from 'next-auth';
+import {NextApiRequest, NextApiResponse} from 'next';
 
 export interface SessionId extends Session {
   user: Session['user'] & {
@@ -10,6 +11,17 @@ export interface SessionId extends Session {
 export type ErrorResponse = {
   error: boolean;
   errorMsg: string;
+};
+
+export type AuthMethodContext = {
+  req: NextApiRequest;
+  res: NextApiResponse;
+  session: SessionId;
+};
+
+export type PublicMethodContext = {
+  req: NextApiRequest;
+  res: NextApiResponse;
 };
 
 export interface DashboardResponse {
