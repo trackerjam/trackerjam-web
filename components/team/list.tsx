35fd04b00c99901430375b73f89d439a71fc2e9c
Denis Leonov <d3liaz@gmail.com>
2023-05-04 14:47:16 +0200

    Draft MemberCard interface

--- components/team/list.tsx
+++ components/team/list.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head';
 import {HeadingSmall as Title, LabelSmall as Subtitle} from 'baseui/typography';
-import {TableBuilder, TableBuilderColumn} from 'baseui/table-semantic';
-import {Button, KIND as ButtonKind, SIZE as ButtonSize} from 'baseui/button';
-import {Tag, KIND} from 'baseui/tag';
+import {Button} from 'baseui/button';
 
 import {useStyletron} from 'baseui';
 import {useMemo} from 'react';
@@ -13,17 +11,18 @@ import {useGetData} from '../hooks/use-get-data';
 import {DEFAULT_TEAM_NAME} from '../../const/team';
 import {ErrorDetails} from '../common/error-details';
 import {TableSkeleton} from './table-skeleton';
-import {CopyableUuid} from './copyable-uuid';
+import {MemberCard} from './member-card';
 
 export function Team() {
   const [css, theme] = useStyletron();
   const {data, isLoading, error} = useGetData<Array<Team & {members: Member[]}>>('/api/team');
   const router = useRouter();
 
-  const tableBlockStyle = css({
-    padding: theme.sizing.scale400,
-    boxShadow: theme.lighting.shadow400,
-    borderRadius: '10px',
+  const cardsBlockWrapper = css({
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 350px))',
+    gap: theme.sizing.scale600,
+    marginBottom: theme.sizing.scale800,
   });
 
   const buttonsBlock = css({
@@ -72,72 +71,14 @@ export function Team() {
         </Button>
       </div>
 
-      <div className={tableBlockStyle}>
-        {isLoading && <TableSkeleton />}
-        {!isLoading && (
-          <TableBuilder
-            data={teamData}
-            overrides={{
-              TableBodyRow: {
-                style: {
-                  ':hover': {
-                    backgroundColor: 'transparent',
-                  },
-                },
-              },
-            }}
-            emptyMessage={
-              <div
-                className={css({
-                  display: 'flex',
-                  alignItems: 'center',
-                  gap: theme.sizing.scale400,
-                })}
-              >
-                <span>No team members yet</span>
-                <Button
-                  kind={ButtonKind.secondary}
-                  size={ButtonSize.mini}
-                  startEnhancer={BsPlusCircle}
-                  onClick={addMemberClickHandler}
-                >
-                  Add
-                </Button>
-              </div>
-            }
-          >
-            <TableBuilderColumn<Member> header="Name">
-              {(row) => {
-                return <div>{row.name}</div>;
-              }}
-            </TableBuilderColumn>
-
-            <TableBuilderColumn<Member> header="Title">
-              {(row) => {
-                return <div>{row.title}</div>;
-              }}
-            </TableBuilderColumn>
-
-            <TableBuilderColumn<Member> header="Token">
-              {(row) => {
-                return <CopyableUuid uuid={row.token} />;
-              }}
-            </TableBuilderColumn>
-
-            <TableBuilderColumn<Member> header="Description">
-              {(row) => row.description?.slice(0, 80) || '-'}
-            </TableBuilderColumn>
-
-            <TableBuilderColumn<Member> header="Status">
-              {(row) => (
-                <Tag kind={KIND.blue} closeable={false}>
-                  {row.status}
-                </Tag>
-              )}
-            </TableBuilderColumn>
-          </TableBuilder>
-        )}
-      </div>
+      {isLoading && <TableSkeleton />}
+      {!isLoading && (
+        <div className={cardsBlockWrapper}>
+          {teamData?.map((userData) => {
+            return <MemberCard key={userData.id} data={userData} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
---
+++ components/team/member-card.tsx
@@ -0,0 +1,176 @@
+import {useStyletron} from 'baseui';
+import {Member} from '@prisma/client';
+import Avatar from 'boring-avatars';
+import {Button, KIND, SIZE, SHAPE} from 'baseui/button';
+import {HiMenu as MenuIcon} from 'react-icons/hi';
+import type {IconType} from 'react-icons';
+import {Tag, KIND as TAG_KIND} from 'baseui/tag';
+import {StatefulPopover} from 'baseui/popover';
+import {StatefulMenu} from 'baseui/menu';
+import {BiCopy, BiEdit, BiTrash} from 'react-icons/bi';
+import {getBorder} from '../../utils/get-border';
+
+interface MenuOptionPros {
+  icon: IconType;
+  label: string;
+  iconColor?: string;
+}
+const MenuOptionIcon = ({icon, label, iconColor}: MenuOptionPros) => {
+  const Icon = icon;
+  const [css, theme] = useStyletron();
+  // const {data, isLoading, error} = useGetData('/api/')
+
+  const boxStyle = css({
+    display: 'flex',
+    gap: theme.sizing.scale300,
+    alignItems: 'center',
+  });
+
+  return (
+    <span className={boxStyle}>
+      <Icon title="" color={iconColor ?? 'inherit'} />
+      {label}
+    </span>
+  );
+};
+
+export function MemberCard({data}: {data: Member}) {
+  const {name, title, status} = data;
+  const [css, theme] = useStyletron();
+
+  const cardStyle = css({
+    position: 'relative',
+    ...theme.borders.border200,
+    padding: theme.sizing.scale600,
+    borderRadius: theme.borders.radius300,
+    boxShadow: theme.lighting.shadow400,
+    transition: '0.2s ease box-shadow',
+    ':hover': {
+      boxShadow: theme.lighting.shadow500,
+    },
+  });
+
+  const nameStyle = css({
+    marginRight: theme.sizing.scale300,
+    fontWeight: 'bold',
+    display: '-webkit-box',
+    '-webkit-line-clamp': 1,
+    '-webkit-box-orient': 'vertical',
+    overflow: 'hidden',
+  });
+
+  const smallTextStyle = css({
+    color: theme.colors.contentTertiary,
+    fontSize: theme.typography.LabelXSmall.fontSize,
+    display: '-webkit-box',
+    '-webkit-line-clamp': 1,
+    '-webkit-box-orient': 'vertical',
+    overflow: 'hidden',
+  });
+
+  const headerLayoutStyle = css({
+    display: 'flex',
+    alignItems: 'flex-start',
+    gap: theme.sizing.scale500,
+  });
+
+  const hrStyle = css({
+    border: 'none',
+    borderTop: getBorder(theme.borders.border200),
+  });
+
+  const avatarStyle = css({
+    flexShrink: 0,
+  });
+
+  return (
+    <div className={cardStyle}>
+      <div className={headerLayoutStyle}>
+        <div className={avatarStyle}>
+          <Avatar
+            size={40}
+            name={`${name}${title ? ' - ' + title : ''}`}
+            variant="beam"
+            colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
+          />
+        </div>
+        <div>
+          <div className={css({display: 'flex'})}>
+            <span className={nameStyle}>{name}</span>
+          </div>
+          <div>
+            <span className={smallTextStyle}>{title}</span>
+          </div>
+        </div>
+
+        <div
+          className={css({
+            justifySelf: 'flex-start',
+            marginBottom: 'auto',
+            alignSelf: 'flex-end',
+            marginLeft: 'auto',
+          })}
+        >
+          <StatefulPopover
+            content={() => (
+              <StatefulMenu
+                items={[
+                  {
+                    label: <MenuOptionIcon icon={BiCopy} label="Copy tracking key" />,
+                  },
+                  {
+                    label: <MenuOptionIcon icon={BiEdit} label="Edit member" />,
+                  },
+                  {divider: true},
+                  {
+                    label: <MenuOptionIcon icon={BiTrash} label="Delete" iconColor="red" />,
+                  },
+                ]}
+              />
+            )}
+            returnFocus
+            autoFocus
+          >
+            <Button
+              title="Member actions"
+              kind={KIND.secondary}
+              shape={SHAPE.circle}
+              size={SIZE.mini}
+              overrides={{
+                BaseButton: {
+                  style: {
+                    top: '10px',
+                    right: '10px',
+                  },
+                },
+              }}
+            >
+              <MenuIcon title="" />
+            </Button>
+          </StatefulPopover>
+        </div>
+      </div>
+
+      <hr className={hrStyle} />
+      <div>
+        <Tag
+          kind={TAG_KIND.blue}
+          closeable={false}
+          overrides={{
+            Root: {
+              style: {
+                marginTop: 0,
+                marginRight: 0,
+                marginBottom: 0,
+                marginLeft: 0,
+                fontSize: '11px',
+              },
+            },
+          }}
+        >
+          {status}
+        </Tag>
+      </div>
+    </div>
+  );
+}
