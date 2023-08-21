ece91952b4cc725791c99ac260e628533ae3deae
Thi Van Le <vannnyle@gmail.com>
2023-08-21 18:28:59 +0200

    Move events page to app folder

--- pages/events/index.tsx
+++
@@ -1,8 +0,0 @@
-import {Events} from '../../components/events';
-import {Layout} from '../../components/layout';
-
-export default function EventsPage() {
-  return <Events />;
-}
-
-EventsPage.Layout = Layout;
