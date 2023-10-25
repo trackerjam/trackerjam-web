013142ea2606125f38d3e1801bccc65417ec0d2a
Denis Leonov <d3liaz@gmail.com>
2023-10-25 22:45:53 +0200

    Restyle score tag

--- components/stats/score-tag.tsx
+++ components/stats/score-tag.tsx
@@ -4,23 +4,27 @@ import {SCORE_LEVEL} from '../../const/productivity';
 export function ScoreTag({score}: {score: number}) {
   let textColor = '';
   let bgColor = '';
+  let borderColor = '';
   let title = '';
 
   if (score > SCORE_LEVEL.GOOD) {
     title = 'High';
     textColor = 'text-green-700';
-    bgColor = 'bg-green-200';
+    bgColor = 'bg-green-50';
+    borderColor = 'border-green-500';
   } else if (score >= SCORE_LEVEL.NEUTRAL) {
     title = 'Neutral';
-    textColor = 'text-orange-700';
-    bgColor = 'bg-orange-200';
+    textColor = 'text-violet-500';
+    bgColor = 'bg-violet-50';
+    borderColor = 'border-violet-300';
   } else {
     title = 'Low';
     textColor = 'text-red-700';
-    bgColor = 'bg-red-200';
+    bgColor = 'bg-red-50';
+    borderColor = 'border-red-500';
   }
 
-  const classes = cx(textColor, bgColor, [
+  const classes = cx(textColor, bgColor, borderColor, [
     'ml-4',
     'text-10',
     'inline-flex',
@@ -31,6 +35,8 @@ export function ScoreTag({score}: {score: number}) {
     'px-2',
     'py-1',
     'rounded-full',
+    'border',
+    'border-solid',
   ]);
 
   return (
