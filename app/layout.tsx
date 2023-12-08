9f186a750e5b565fafb1ec78ef34e7ba3ae714c2
Denis Leonov <d3liaz@gmail.com>
2023-12-08 21:57:54 +0100

    Delete unused _app and _document. Add plausible.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
+import PlausibleProvider from 'next-plausible';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <PlausibleProvider domain="app.trackerjam.com" customDomain="https://p.hostedapp.one" />
       </head>
       <body>{children}</body>
     </html>
