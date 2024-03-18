a5da4242cb4211fcd629de66a2e99ba286edbeb8
Denis Leonov <d3liaz@gmail.com>
2024-03-18 23:09:49 +0100

    Improve script

--- scripts/fetch-new-domains.ts
+++ scripts/fetch-new-domains.ts
@@ -6,9 +6,10 @@ import {isIp} from '../utils/is-ip';
 
 const classifiedDomains = Object.keys(DOMAIN_TAGS);
 
-const excludedDomains = ['googleusercontent.com', 'amazonaws.com'];
+const excludedDomains = ['googleusercontent.com', 'amazonaws.com', '.vercel.app', '.specha.co'];
 
 const prismadb = new PrismaClient();
+
 async function fetchNewDomains() {
   const domainRecords = await prismadb.domain.findMany({});
   const domains = domainRecords.map((record: any) => record.domain);
@@ -17,6 +18,14 @@ async function fetchNewDomains() {
     return !classifiedDomains.includes(domain) && !excludedDomains.some((d) => domain.endsWith(d));
   });
   const unClassifiedDomains = Array.from(new Set(newDomains.filter((domain) => !isIp(domain))));
+  const unknownDomains = Object.entries(DOMAIN_TAGS)
+    .reduce((acc, [domain, tags]) => {
+      if (tags.Unknown === 1 || tags.Other === 1) {
+        acc.push(domain);
+      }
+      return acc;
+    }, [] as string[])
+    .filter((domain) => !excludedDomains.some((d) => domain.endsWith(d)));
 
   newDomains.sort();
 
@@ -24,12 +33,15 @@ async function fetchNewDomains() {
   console.log('Total domains in DB:', domains.length);
   console.log('Excluded IP addresses:', newDomains.length - unClassifiedDomains.length);
   console.log('Found unclassified domains:', unClassifiedDomains.length);
+  console.log('Found unknown domains:', unknownDomains.length);
 
   writeFileSync(
     join(__dirname, '/unclassified-domains.txt'),
     unClassifiedDomains.join('\n'),
     'utf8'
   );
+
+  writeFileSync(join(__dirname, '/unknown-domains.txt'), unknownDomains.join('\n'), 'utf8');
 }
 
 fetchNewDomains();
