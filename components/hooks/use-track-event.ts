79a6d6f4330d18fadc63895931217b9a2e28e726
Denis Leonov <d3liaz@gmail.com>
2024-04-09 22:02:27 +0200

    Exclude admins from event tracking by sha1 hash of user id

--- components/hooks/use-track-event.ts
+++ components/hooks/use-track-event.ts
@@ -1,6 +1,13 @@
 import {usePlausible} from 'next-plausible';
 import {useSession} from 'next-auth/react';
+import {sha1} from 'js-sha1';
 import {shortenUUID} from '../../utils/shorten-uuid';
+
+// Excluded admins
+const EXCLUDED_SHA1_USER_IDS: string[] = [
+  '4398424aaadf12783e951e3dae8337ee8eb5dcf5',
+  '46a17c807be900110c1cd68fddf484258c0eeb4e',
+];
 
 export function useTrackEvent() {
   const plausible = usePlausible();
@@ -8,6 +15,13 @@ export function useTrackEvent() {
 
   return (eventName: string, ...params: unknown[]) => {
     const usedId = session?.user?.id || '<unknown>';
+
+    const userIdSha1 = sha1(usedId);
+    if (EXCLUDED_SHA1_USER_IDS.includes(userIdSha1)) {
+      console.info('[Excluded Event]:', eventName, ...params);
+      return;
+    }
+
     const shortId = shortenUUID(usedId);
 
     plausible(eventName, {
