6ae9816638e9b8ec54986cb323f0b7e2d7a0cdc4
Denis Leonov <d3liaz@gmail.com>
2024-04-22 21:55:48 +0200

    Update sent emails

--- scripts/send-question-emails.ts
+++ scripts/send-question-emails.ts
@@ -2,46 +2,85 @@ import 'dotenv/config';
 import nodemailer from 'nodemailer';
 
 const sent = [
-  'vannnyle@gmail.com',
-  'almas.abulkhairov@gmail.com',
-  'nompo4ta@gmail.com',
-  'smyk.oa@gmail.com',
-  'kartikeyj123@gmail.com',
-  'msaco.testacc@gmail.com',
-  'td.eliteclass@gmail.com',
-  'ebenezerrahul@gmail.com',
-  'castlewania.alex@gmail.com',
-  'ahmmburr.rose@gmail.com',
-  'hello@filip.us',
-  'cbuspri@outlook.com',
+  '',
+  // 'vannnyle@gmail.com',
+  // 'almas.abulkhairov@gmail.com',
+  // 'nompo4ta@gmail.com',
+  // 'smyk.oa@gmail.com',
+  // 'kartikeyj123@gmail.com',
+  // 'msaco.testacc@gmail.com',
+  // 'td.eliteclass@gmail.com',
+  // 'ebenezerrahul@gmail.com',
+  // 'castlewania.alex@gmail.com',
+  // 'ahmmburr.rose@gmail.com',
+  // 'hello@filip.us',
+  // 'cbuspri@outlook.com',
+  // 'airobot67@gmail.com',
+  // 'decorkkmc@gmail.com',
+  // 'chahalvishal000@gmail.com',
+  // 'brizhaclaudiaatoch@gmail.com',
+  // 'qerqt22@gmail.com',
+  // 'ishii@fmi.co.jp',
+  // 'lizheng8318@gmail.com',
+  // 'pawankumaragarwal1782@gmail.com',
+  // 'vannnyle@gmail.com',
+  // 'almas.abulkhairov@gmail.com',
+  // 'nompo4ta@gmail.com',
+  // 'smyk.oa@gmail.com',
+  // 'kartikeyj123@gmail.com',
+  // 'msaco.testacc@gmail.com',
+  // 'td.eliteclass@gmail.com',
+  // 'ebenezerrahul@gmail.com',
+  // 'ahmmburr.rose@gmail.com',
+  // 'hello@filip.us',
+  // 'cbuspri@outlook.com',
+  // 'aravindnirmal10@gmail.com',
+  // 'shradhadalvi3051@gmail.com',
+  // 'anushreeshekhar120@gmail.com',
+  // 'iblondin@tink.ca',
+  // 'aadiljannath666@gmail.com',
+  // 'ahmmburr.rose@gmail.com',
+  // 'castlewania.alex+@gmail.com',
+  // 'd3liaz@yandex.com',
+  // 'ebenezerrahul@gmail.com',
+  // 'gabrielmorenoibarra@gmail.com',
+  // 'hatredforlife66@gmail.com',
+  // 'iblondin@tink.ca',
+  // 'kartikeyj123@gmail.com',
+  // 'msaco.testacc@gmail.com',
+  // 'qerqt22@gmail.com',
+  // 'qertest09@gmail.com',
+  // 'rjonesfisherman@gmail.com',
+  // 'shradhadalvi3051@gmail.com',
+  // 'smyk.oa@gmail.com',
+  // 'soshi.nakama@onedrops.com',
+  // 'tim.hein@victoriousseo.com',
 ];
 
 const allUsers = [
-  'airobot67@gmail.com',
-  'decorkkmc@gmail.com',
-  'chahalvishal000@gmail.com',
-  'brizhaclaudiaatoch@gmail.com',
-  'qerqt22@gmail.com',
-  'ishii@fmi.co.jp',
-  'lizheng8318@gmail.com',
-  'pawankumaragarwal1782@gmail.com',
-  'vannnyle@gmail.com',
-  'almas.abulkhairov@gmail.com',
-  'nompo4ta@gmail.com',
-  'smyk.oa@gmail.com',
-  'kartikeyj123@gmail.com',
-  'msaco.testacc@gmail.com',
-  'td.eliteclass@gmail.com',
-  'ebenezerrahul@gmail.com',
-  'd3liaz@gmail.com',
+  // 'd3liaz@gmail.com',
   'castlewania.alex@gmail.com',
-  'ahmmburr.rose@gmail.com',
-  'hello@filip.us',
-  'cbuspri@outlook.com',
-  'aravindnirmal10@gmail.com',
-  'shradhadalvi3051@gmail.com',
+  'aadiljannath007@gmail.com',
+  'teampodteam@gmail.com',
+  'mintmnr@gmail.com',
+  'kaichiny@gmail.com',
+  'hemanthsairaj009@gmail.com',
+  'sunil.p112233@gmail.com',
+  'krishna427549@gmail.com',
+  'real.wh1t30@gmail.com',
+  'khaledapanna1617@gmail.com',
+  'onekertis@gmail.com',
+  'pawankumaragarwal1782@gmail.com',
+  'lizheng8318@gmail.com',
+  'ishii@fmi.co.jp',
+  'brizhaclaudiaatoch@gmail.com',
+  'chahalvishal000@gmail.com',
+  'decorkkmc@gmail.com',
   'anushreeshekhar120@gmail.com',
-  'iblondin@tink.ca',
+  'aravindnirmal10@gmail.com',
+  'cbuspri@outlook.com',
+  'hello@filip.us',
+  'td.eliteclass@gmail.com',
 ];
 
 function getHtml() {
@@ -84,17 +123,16 @@ function getHtml() {
 <main>
     <p class="content">
         Hi,
-        <br/> This is Denis from TrackerJam.com ⏰
-        <br/> I hope you are doing well!
+        <br/> This is Denis from TrackerJam.com
     </p>
 
     <p class="content">
-        It's been some time now since you joined the TrackerJam and I would love to hear what you think about our web
-        tracker.
-        <br/>Does it help you track your web activity? What features do you miss?
+        I see you signed up for TrackerJam but haven't installed the extension yet.
+        <br>
+        Let&apos;s set up a quick call to help you get started!
     </p>
     <p class="content">
-        Let me know if you want to have a video call to discuss it.
+        Feel free to pick a time slot that works for you: <a href="https://calendly.com/d3liaz/25min">Calendly</a>
     </p>
 </main>
 
@@ -105,8 +143,7 @@ function getHtml() {
         Denis from <a href="https://trackerjam.com/">TrackerJam.com</a>
     </p>
     <p class="unsubscribe">
-        You are receiving this email because you signed up for TrackerJam. If you don't want to receive these emails in
-        the future, you can <a href="%unsubscribe_url%">unsubscribe</a>.
+        You are receiving this email because you signed up for TrackerJam. <a href="%unsubscribe_url%">Unsubscribe</a>.
     </p>
 
 </footer>
@@ -126,10 +163,10 @@ export async function sendEmail(to: string) {
 
   return transporter.sendMail({
     to,
-    from: 'TrackerJam <hi@trackerjam.com>',
+    from: 'Denis L <hi@trackerjam.com>',
     replyTo: 'hi@trackerjam.com',
     sender: 'Denis from TrackerJam',
-    subject: 'What do you think?', // Subject line
+    subject: 'Call about TrackerJam', // Subject line
     text: '', // plain text body
     html: getHtml(),
   });
@@ -137,6 +174,10 @@ export async function sendEmail(to: string) {
 
 (async () => {
   const emailsToSend = allUsers.filter((email) => !sent.includes(email));
+  const filteredEmails = allUsers.length - emailsToSend.length;
+  if (filteredEmails !== 0) {
+    console.log('Filtered emails: ', filteredEmails);
+  }
 
   for (const receiver of emailsToSend) {
     console.log('Sending email to: ', receiver);
